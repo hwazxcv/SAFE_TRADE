529ec25866e0d13e42ac5a1be92de6d39afdd0ba
[hhs] <hwazxcv@naver.com>
2023-11-06 19:03:44 +0900

    테스트

---
+++ src/main/java/com/example/git_test/GitTestApplication.java
@@ -0,0 +1,14 @@
+package com.example.git_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//테스트
+@SpringBootApplication
+public class GitTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitTestApplication.class, args);
+    }
+
+}
